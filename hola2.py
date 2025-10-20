f35e2261df379c8023e42b3742f03a7e49254c40
quispeliviasgerson-cpu <quispeliviasgerson@gmail.com>
2025-10-19 22:15:01 -0500

    Update hola2.py

--- hola2.py
+++ hola2.py
@@ -1,2 +1,17 @@
 print("hola mundo")
-print("todoestabien")
+print("todo esta bien")
+suma=10+777
+resta=567-98
+multiplicacion=2*77
+division=99/12
+resto=500%6
+div_piso=10//3
+exp=6**4
+#prints
+print("R_Suma:",suma)
+print("R_Resta:",resta)
+print("R_Producto:",multiplicacion)
+print("R_Division:",division)
+print("R_Residuo:",resto)
+print("R_Division piso:",div_piso)
+print("R_Potencia:",exp)
